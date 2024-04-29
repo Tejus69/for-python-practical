0ae72392176caceb02b1993a255021d29c0150c1
Tejas_illustrator <tejshinde2005@gmail.com>
2024-04-29 21:30:20 +0530

    Update ques1.py

--- ques1.py
+++ ques1.py
@@ -1,31 +1,31 @@
 # #to understand Different List and tuple Operations using builtin functions
 
-# list = [2,1,3,4]
-# list2=[7,8,9]
+list = [2,1,3,4]
+list2=[7,8,9]
 
-# print("List at a start")
-# print(list)
+print("List at a start")
+print(list)
 
-# #adding element in list 
-# list.append(5)
-# list.insert(4,6)#it inserts at a particular index
-# list.extend(list2)
-# print("List after Inserting")
-# print(list)
+#adding element in list 
+list.append(5)
+list.insert(4,6)#it inserts at a particular index
+list.extend(list2)
+print("List after Inserting")
+print(list)
 
 
-# #Removing 
-# list.remove(7)
-# list.pop()
-# print("list after Removing")
-# print(list)
+#Removing 
+list.remove(7)
+list.pop()
+print("list after Removing")
+print(list)
 
-# #sorting opertaion on a list 
+#sorting opertaion on a list 
 
-# list.sort() # isse se by default ascending order me print hoga
-# print(list)
-# list.sort(reverse=True) # descending
-# print(list)
+list.sort() # isse se by default ascending order me print hoga
+print(list)
+list.sort(reverse=True) # descending
+print(list)
 
 
 #Tupple
